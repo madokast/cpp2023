20b1fdfd8ce66063fae8158350843d620a60afa8
madokast <578562554@qq.com>
2023-04-15 12:12:22 +0800

    vs runable

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <algorithm>
+#include <array>
 
 auto main() -> int {
-    std::array a {1,2,3};
+    std::array a {1,2,3,4};
     std::for_each(a.begin(), a.end(), [](const auto e) {
         std::cout << e << std::endl;
     });
+    for (const auto item: a) {
+        std::printf("%d\n", item);
+    }
 }
